f22693ea92410111c3d194fa467774305f3a1e9c
anaismariacr <am.castro23@uniandes.edu.co>
2025-04-27 17:35:33 -0500

    base agendar servicio

--- src/main/java/uniandes/edu/co/proyecto/services/AgendarServicio.java
+++ src/main/java/uniandes/edu/co/proyecto/services/AgendarServicio.java
@@ -1,5 +1,33 @@
 package uniandes.edu.co.proyecto.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import jakarta.transaction.Transactional;
+import uniandes.edu.co.proyecto.modelo.Cita;
+import uniandes.edu.co.proyecto.modelo.ServicioSalud;
+import uniandes.edu.co.proyecto.repositorio.CitaRepository;
+import uniandes.edu.co.proyecto.repositorio.OrdenServicioRepository;
+
 public class AgendarServicio {
-    
+    @Autowired
+    private OrdenServicioRepository ordenRepo;
+
+    @Autowired
+    private CitaRepository citaRepo;
+
+    @Transactional
+    public String agendarCita(ServicioSalud servicio, Cita cita){
+
+        return "No hay nada";
+    }
+
+    @Transactional
+    public Boolean consultarDispSerializable(){
+        return true;
+    }
+
+    @Transactional
+    public Boolean consultarDispReadCommited(){
+        return true;
+    }
 }
